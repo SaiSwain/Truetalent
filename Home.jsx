99e003552fee609b32550b9c8770fe8e8ee62672
SaiSwain <132897931+SaiSwain@users.noreply.github.com>
2023-05-11 00:07:52 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,46 @@
+import React from "react"
+const Home=()=>{
+    return(
+        <div>
+           <form>
+           <div>
+            <input type="checkbox" />
+            <label>Mumbai, Maharashtra</label>
+          </div>
+          <div>
+            <input type="checkbox" />
+            <label>Bangalore, Karnataka</label>
+          </div>
+          <div>
+            <input type="checkbox" />
+            <label>New Delhi, Delhi</label>
+          </div>
+          <div>
+            <input type="checkbox" />
+            <label>Kolkata, West Bengal</label>
+          </div>
+          <div>
+            <input type="checkbox" />
+            <label>Chennai, Tamil Nadu</label>
+          </div>
+          <div>
+            <input type="checkbox" />
+            <label>Pune, Maharashtra</label>
+          </div>
+          <div>
+            <input type="checkbox" />
+            <label>Indore, Madhya Pradesh</label>
+          </div>
+          <div>
+            <input type="checkbox" />
+            <label>Ahmedabad, Gujarat</label>
+          </div>
+          <div>
+            <input type="checkbox" />
+            <label>Bhubaneswar, Odisha</label>
+          </div>
+           </form>
+        </div>
+    )
+}
+export default Home;
